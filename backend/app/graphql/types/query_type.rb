3243b4290ae38acb9cb70a0bbb825a7531dda975
munirkizilcali <munir.kizilcali@gmail.com>
2018-10-01 16:35:12 -0400

    debugging on cors error

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -53,13 +53,13 @@ Types::QueryType = GraphQL::ObjectType.define do
     resolve -> (obj, args, ctx) { [ctx[:current_user]] }
   end
 
-  field :children, !types[Types::BabyType] do  
-    resolve -> (obj, args, ctx) { [ctx[:current_user].children] }
-  end
+  # field :children, !types[Types::BabyType] do  
+  #   resolve -> (obj, args, ctx) { [ctx[:current_user].children] }
+  # end
 
-  field :babies, !types[Types::BabyType] do  
-    resolve -> (obj, args, ctx) { [ctx[:current_user].babies] }
-  end
+  # field :babies, !types[Types::BabyType] do  
+  #   resolve -> (obj, args, ctx) { [ctx[:current_user].babies] }
+  # end
 
   # field :allEvents, !types[Types::EventType] do 
   # 	resolve -> (obj, args, ctx) { [ctx[:current_user].events]}
