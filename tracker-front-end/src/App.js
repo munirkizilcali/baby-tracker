19833d61c13c645426dafdf2876f130a10833629
munirkizilcali <munir.kizilcali@gmail.com>
2018-10-03 10:12:42 -0400

    animate.css experimenting with bootstrap components

--- tracker-front-end/src/App.js
+++ tracker-front-end/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -17,9 +20,19 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+           tabi Learn React
           </a>
-        </header>
+
+        <div className="dropdown .visible-xs">
+          <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onClick={()=>this.setState({ show: !this.state.show })}>
+            Dropdown button
+          </button>
+          <div className='dropdown-menu show animated fadeIn .visible-md-inline' aria-labelledby="dropdownMenuButton">
+            <a className="dropdown-item" href="#">Action</a>
+            <a className="dropdown-item" href="#">Another action</a>
+            <a className="dropdown-item" href="#">Something else here</a>
+          </div>
+        </div>
       </div>
     );
   }
