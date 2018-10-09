d32acd7852bd5ca622fa86f0722fa14efd90e54f
munirkizilcali <munir.kizilcali@gmail.com>
2018-10-08 20:24:55 -0400

    first apollo fetch done

--- tracker-front-end/src/App.js
+++ tracker-front-end/src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
+import { createApolloFetch } from 'apollo-fetch';
 
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      show: false
+      show: false,
+      userInfo: null 
     }
+  }
+
+  componentDidMount() {
+
+    const uri = 'http://localhost:3000/graphql';
+    const authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MzkxMjkzMDYsInN1YiI6MX0.wDDpTMSYXf2ssazKtOwOEPRPVat1SfMNk15jD0ZmXok"
+    const query = ` 
+                  {
+                    currentUser {
+                      id
+                      name
+                      children {
+                        id
+                      }
+                    }
+                  }
+                  `
+
+    const apolloFetch = createApolloFetch({ uri });
+
+    apolloFetch.use(({ request, options }, next) => {
+      if (!options.headers) {
+        options.headers = {};  // Create the headers object if needed.
+      }
+      options.headers['authorization'] = authorization;
+
+      next();
+    });
+    console.log(query)
+    apolloFetch({query}).then(resp=>this.setState({userInfo: resp.data}))
+
+
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
   }
   render() {
     return (
@@ -22,7 +60,9 @@ class App extends Component {
           >
            tabi Learn React
           </a>
+      {this.state.userInfo ? JSON.stringify(this.state.userInfo) : ''}
       </div>
+
     );
   }
 }
