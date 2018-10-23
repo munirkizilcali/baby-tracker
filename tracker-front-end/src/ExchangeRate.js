0dafcc81a3d0648a9c3c1887962f99cbd3df7059
munirkizilcali <munir.kizilcali@gmail.com>
2018-10-22 21:37:52 -0400

    using local backend for queries with authentication token

--- tracker-front-end/src/ExchangeRate.js
+++ tracker-front-end/src/ExchangeRate.js
@@ -3,19 +3,18 @@ import withApollo from "./withApollo";
 
 class ExchangeRate extends React.Component {
 	render() {
-		return this.props.data.rates.map(({ currency, rate }) => (
-			<div key={currency}>
-				<p>{`${currency}: ${rate}`}</p>
-			</div>
-		));
+		return <div>{JSON.stringify(this.props.data)}</div>;
 	}
 }
 
 export default withApollo(`
-					{
-						rates(currency: "USD") {
-							currency
-							rate
-						}
-					}
+							{
+							  currentUser {
+							    id
+							    name
+							    children {
+							      id
+							    }
+							  }
+							}
 				`)(ExchangeRate);
--- tracker-front-end/src/index.js
+++ tracker-front-end/src/index.js
@@ -7,8 +7,17 @@ import "bootstrap/dist/css/bootstrap.css";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
+// const client = new ApolloClient({
+// 	uri: "https://w5xlvm3vzz.lp.gql.zone/graphql",
+// 	headers: {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDAzNDQ0MTcsInN1YiI6MX0.dHZcGN13r9H8flDj_f3erjMeq7AEd4BccJFq2i7xf8w"}
+// });
+
 const client = new ApolloClient({
-	uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+	uri: "http://localhost:3000/graphql",
+	headers: {
+		Authorization:
+			"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NDAzNDQ0MTcsInN1YiI6MX0.dHZcGN13r9H8flDj_f3erjMeq7AEd4BccJFq2i7xf8w"
+	}
 });
 
 ReactDOM.render(
